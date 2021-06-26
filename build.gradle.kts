a5184c1b0c6efbce7750ae9a872b3a956a0396c9
lukstbit <lukstbit@users.noreply.github.com>
2021-06-26 14:55:50 +0300

    Fix project setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ buildscript {
     dependencies {
         classpath ("com.android.tools.build:gradle:4.2.1")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
-
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -17,6 +16,5 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        jcenter() // Warning: this repository is going to shut down soon
     }
 }
