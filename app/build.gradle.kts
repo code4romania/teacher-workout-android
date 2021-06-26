47bc580f898c259128df2921a9b56460072e481e
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2021-06-26 15:27:06 +0300

    Finish migration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,27 +1,47 @@
 plugins {
-    `java-gradle-plugin`
-    `kotlin-dsl`
-    `kotlin-dsl-precompiled-script-plugins`
+    id("com.android.application")
+    id("kotlin-android")
 }
 
-repositories {
-    google()
-    jcenter()
-    mavenCentral()
-    maven("https://plugins.gradle.org/m2/")
-}
+android {
+    compileSdkVersion(30)
+    buildToolsVersion("30.0.3")
 
-object PluginsVersions {
-    const val KOTLIN = "1.5.20"
+    defaultConfig {
+        applicationId("com.example.teacher_workout")
+        minSdkVersion(24)
+        targetSdkVersion(30)
+        versionCode(1)
+        versionName("1.0")
+
+        testInstrumentationRunner("androidx.test.runner.AndroidJUnitRunner")
+    }
+
+    buildTypes {
+        getByName("release") {
+            minifyEnabled(false)
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility(JavaVersion.VERSION_1_8)
+        targetCompatibility(JavaVersion.VERSION_1_8)
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:${PluginsVersions.KOTLIN}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.20")
     implementation("androidx.core:core-ktx:1.5.0")
     implementation("androidx.appcompat:appcompat:1.3.0")
     implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
     testImplementation("junit:junit:4.+")
-    testImplementation("androidx.test.ext:junit:1.1.2")
-    testImplementation("androidx.test.espresso:espresso-core:3.3.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
 }
