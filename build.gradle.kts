b049801590a4a6310e26043fac7e580ae9709fd7
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2021-07-18 21:46:49 +0300

    Improvements after adding dynamic features (#20)
    * Fix JDK version
    
    * Fix kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val kotlin_version by extra("1.5.10")
+    val kotlinVersion by extra("1.5.20")
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath ("com.android.tools.build:gradle:4.2.1")
-        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
+        classpath ("com.android.tools.build:gradle:4.2.2")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts.kts files
     }
