8112a40e161a188066aa68fbce64aefae2225e49
Calinoiu Alexandru Nicolae <calinoiu.alexandru@agilefreaks.com>
2021-09-25 14:02:33 +0300

    Add small test class for viewModle (#28)
    * Add small test class for viewModle
    
    * Intentional test failure to test the ci
    
    * Fix failing test

---
+++ features/account/src/test/java/com/teacherworkout/features/account/auth/AuthViewModelTest.kt
@@ -0,0 +1,31 @@
+package com.teacherworkout.features.account.auth
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class AuthViewModelTest {
+    @Test
+    fun `setInitialState will set the initial state`() {
+        val viewModel = AuthViewModel()
+
+        assertThat(viewModel.setInitialState()).isEqualTo(AuthContract.State())
+    }
+
+    @Test
+    fun `handleEvents will handle SetEmail`() {
+        val viewModel = AuthViewModel()
+
+        viewModel.handleEvents(AuthContract.Event.SetEmail("ion@code4.ro"))
+
+        assertThat(viewModel.viewState.value.email).isEqualTo("ion@code4.ro")
+    }
+
+    @Test
+    fun `handleEvents will handle SetPassword`() {
+        val viewModel = AuthViewModel()
+
+        viewModel.handleEvents(AuthContract.Event.SetPassword("horsestampler"))
+
+        assertThat(viewModel.viewState.value.password).isEqualTo("horsestampler")
+    }
+}
