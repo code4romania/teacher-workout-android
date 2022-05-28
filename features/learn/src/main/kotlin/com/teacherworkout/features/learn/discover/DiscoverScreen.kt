9df12e07caacfce4d2022c47d480070c7705cb46
Horațiu Udrea <udrea.horatiu@gmail.com>
2022-05-28 14:28:46 +0300

    Fix learn feature linter warnings

--- features/learn/src/main/kotlin/com/teacherworkout/features/learn/discover/DiscoverScreen.kt
+++ features/learn/src/main/kotlin/com/teacherworkout/features/learn/discover/DiscoverScreen.kt
@@ -2,10 +2,10 @@ package com.teacherworkout.features.learn.discover
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,14 +16,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.teacherworkout.commons.ui.composables.HandleEffects
-import com.teacherworkout.commons.ui.composables.lessonThemesItem
 import com.teacherworkout.commons.ui.composables.SearchView
+import com.teacherworkout.commons.ui.composables.lessonThemesItem
 import com.teacherworkout.commons.ui.data.impl.FakeLessonsRepository
 import com.teacherworkout.features.learn.R
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.onEach
 
 private val listState = LazyListState()
 
@@ -34,7 +32,6 @@ fun DiscoverScreen(
     effects: Flow<DiscoverContract.Effect>,
     onNavigationRequest: (DiscoverContract.Effect.Navigation) -> Unit
 ) {
-    val space8dp = dimensionResource(id = R.dimen.space_8dp)
     val space16dp = dimensionResource(id = R.dimen.space_16dp)
 
     val snackbarHostState = remember { SnackbarHostState() }
@@ -59,37 +56,13 @@ fun DiscoverScreen(
             verticalArrangement = Arrangement.spacedBy(space16dp),
             state = listState
         ) {
-            item {
-                Spacer(modifier = Modifier.height(space16dp))
-                Text(
-                    text = stringResource(id = R.string.discover_lessons_title),
-                    style = TextStyle(
-                        color = MaterialTheme.colors.primary,
-                        fontSize = MaterialTheme.typography.h4.fontSize,
-                        fontWeight = FontWeight.Bold
-                    )
-                )
-                Spacer(modifier = Modifier.height(space8dp))
-                SearchView(
-                    modifier = Modifier.fillMaxWidth(),
-                    searchInput = state.searchInput,
-                    onSearchInputChange = {
-                        onSendEvent(DiscoverContract.Event.SetSearchInput(it))
-                    },
-                    onClear = {
-                        onSendEvent(DiscoverContract.Event.SetSearchInput(TextFieldValue("")))
-                    },
-                    placeholderText = stringResource(id = R.string.search_label)
-                )
-                Spacer(modifier = Modifier.height(space16dp))
-            }
-            if(state.isLoading) {
+            titleAndSearchBarItem(state, onSendEvent)
+            if (state.isLoading) {
                 item {
                     Box(
-                        //TODO: look for a better way to center the progress bar
                         modifier = Modifier
                             .fillMaxWidth()
-                            .fillParentMaxHeight(0.5f),
+                            .fillParentMaxHeight(fraction = 0.5f),
                         contentAlignment = Alignment.Center
                     ) {
                         CircularProgressIndicator()
@@ -111,6 +84,39 @@ fun DiscoverScreen(
             modifier = Modifier.align(Alignment.BottomCenter),
             hostState = snackbarHostState
         )
+    }
+}
+
+private fun LazyListScope.titleAndSearchBarItem(
+    state: DiscoverContract.State,
+    onSendEvent: (DiscoverContract.Event) -> Unit
+) {
+    item {
+        val space8dp = dimensionResource(id = R.dimen.space_8dp)
+        val space16dp = dimensionResource(id = R.dimen.space_16dp)
+
+        Spacer(modifier = Modifier.height(space16dp))
+        Text(
+            text = stringResource(id = R.string.discover_lessons_title),
+            style = TextStyle(
+                color = MaterialTheme.colors.primary,
+                fontSize = MaterialTheme.typography.h4.fontSize,
+                fontWeight = FontWeight.Bold
+            )
+        )
+        Spacer(modifier = Modifier.height(space8dp))
+        SearchView(
+            modifier = Modifier.fillMaxWidth(),
+            searchInput = state.searchInput,
+            onSearchInputChange = {
+                onSendEvent(DiscoverContract.Event.SetSearchInput(it))
+            },
+            onClear = {
+                onSendEvent(DiscoverContract.Event.SetSearchInput(TextFieldValue("")))
+            },
+            placeholderText = stringResource(id = R.string.search_label)
+        )
+        Spacer(modifier = Modifier.height(space16dp))
     }
 }
 
