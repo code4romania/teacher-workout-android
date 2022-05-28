24c266e5408b1ccd3f990c4504c78efd1752dcdd
calin <calinoiu.alexandru@agilefreaks.com>
2022-05-28 12:05:45 +0300

    Remove more magic numbers

--- features/profile/src/main/kotlin/com/teacherworkout/features/profile/results/ResultsTabContent.kt
+++ features/profile/src/main/kotlin/com/teacherworkout/features/profile/results/ResultsTabContent.kt
@@ -7,17 +7,31 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.teacherworkout.features.profile.R
 
+const val COMPLETED_LESSONS = 16
+const val TOTAL_LESSONS = 42
+
+const val COMPLETED_LESSONS_1 = 10
+const val COMPLETED_LESSONS_2 = 20
+
 @Composable
 fun ResultsTabContent(lessonsCategories: List<String>) {
     LazyColumn(modifier = Modifier.fillMaxWidth()) {
         item {
-            LessonRow(lessonCategoryText = stringResource(id = R.string.label_lessons_finished), 16, 210) {
+            LessonRow(
+                lessonCategoryText = stringResource(id = R.string.label_lessons_finished),
+                COMPLETED_LESSONS,
+                TOTAL_LESSONS
+            ) {
                 /* TODO */
             }
         }
         lessonsCategories.forEachIndexed { index, value ->
             item {
-                LessonRow(lessonCategoryText = value, if (index == 2) 41 else 26, 42) {
+                LessonRow(
+                    lessonCategoryText = value,
+                    if (index == 2) COMPLETED_LESSONS_1 else COMPLETED_LESSONS_2,
+                    TOTAL_LESSONS
+                ) {
                     /* TODO */
                 }
             }
